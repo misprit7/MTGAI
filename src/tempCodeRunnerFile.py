688b442cee98b7aff796d929518f79ecdabeee50
xander <xandernaumenko@gmail.com>
2020-08-23 00:11:31 -0700

    Fixed up gitignore

--- src/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-turechoice = next(x for x in choices['actions'] if x['actionType'] == 'ActionType_Cast' and game.gameObject(x['grpId']).type == 'CardType_Creature')
-
-        cardnum = next(i for i, x in enumerate(game.hand()) if x.grpId == creaturechoice['grpId'])
-
-        prin
