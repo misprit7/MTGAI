4e3d23fbaab15fdf4b82e6e4018cb6744a2f3995
Xander Naumenko <33139843+misprit7@users.noreply.github.com>
2020-12-16 11:56:13 -0800

    Delete MTGAWithData.bat

--- MTGAWithData.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-copy "C:\Users\xander\AppData\LocalLow\Wizards Of The Coast\MTGA\Player.log" ".\data\%DATE%-%TIME:~0,2%-%TIME:~3,2%.log"
-"C:\Program Files\Wizards of the Coast\MTGA\MTGALauncher\MTGALauncher.exe"
